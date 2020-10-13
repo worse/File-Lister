2e8428d215cd4d14a3f421509be14dd2f2c75638
worse <skita@protonmail.com>
2020-10-13 03:22:44 -0700

    Added file extension filtering

--- src/main/java/al/crimin/Main.java
+++ src/main/java/al/crimin/Main.java
@@ -8,14 +8,23 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Stream.of;
+
 public class Main {
 
     @SneakyThrows
     public static void main(String[] args) {
-        List<Path> results = Files.list(Paths.get("D:/Music/Unreleased/juicewrld"))
+
+        List<Path> results = Files.list(Paths.get(System.getProperty("user.dir")))
                 .map(Path::getFileName)
                 .collect(Collectors.toList());
-        results.forEach(System.out::println);
+
+        String[] extensionTypes = {".txt", ".csv", ".txt", ".sql"};
+
+        results.stream().filter(
+                result -> of(extensionTypes).anyMatch(s -> result.toString().endsWith(s))
+        ).forEach(System.out::println);
+
     }
 
 }
